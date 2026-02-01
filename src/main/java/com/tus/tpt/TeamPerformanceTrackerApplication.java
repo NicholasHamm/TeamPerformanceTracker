6a0a9dd854f083962104fff340cc3c4c258d6153
Nicholas Hamm <hamm.nicholasj@gmail.com>
2026-02-01 22:48:24 +0000

    Skeleton Spring Boot structure.

---
+++ src/main/java/com/tus/tpt/TeamPerformanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tus.tpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPerformanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamPerformanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tus/tpt/TeamPerformanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.tpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPerformanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
